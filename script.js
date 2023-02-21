ca8718c70f969780d3c24b054551fed76ae68c17
AaronSicard95 <aaron.sicard95@gmail.com>
2023-02-21 18:57:15 -0500

    Added Remove
    Added Remove button to list items and fixed numOfItems local storage error

--- script.js
+++ script.js
@@ -14,10 +14,21 @@ function clickedUndo(dex){
     myList.children[dex].classList = "notDone";
     localStorage.setItem(`isDone${dex}`, "notDone");
 }
+//Removes List Item At Index
+function clickedRemove(dex){
+    let lastIndex = parseInt(localStorage.getItem("numOfItems"));
+    for(let i = dex; i<lastIndex;i++){
+        localStorage.setItem(`info${i}`, localStorage.getItem(`info${i+1}`));
+        localStorage.setItem(`isDone${i}`, localStorage.getItem(`isDone${i+1}`));
+        console.log(localStorage.getItem(`info${i}`));
+    }
+    localStorage.setItem("numOfItems", parseInt(localStorage.getItem("numOfItems"))-1);
+    myList.children[dex].remove();
+}
 //Puts list Items onto the page
 function addToList(index){
     let newItem = document.createElement("li");
-    newItem.innerHTML = `${localStorage.getItem(`info${index}`)}    <input id="buttonFor${index}" type="Button" value="complete">  <input id="undoButtonFor${index}" type="Button" value="Not Done">`;
+    newItem.innerHTML = `${localStorage.getItem(`info${index}`)}    <input id="buttonFor${index}" type="Button" value="complete">  <input id="undoButtonFor${index}" type="Button" value="Not Done">    <input id="removeButtonFor${index}" type="Button" value="Remove">`;
     newItem.id = `${index}`;
     let checkDone = `isDone${index}`;
     newItem.classList = localStorage.getItem(checkDone);
@@ -26,9 +37,10 @@ function addToList(index){
 //add new list item to sttorage and the page once form is Submitted
 function submitFunc(evt){
     evt.preventDefault();
+    localStorage.setItem(`info${parseInt(localStorage.getItem("numOfItems"))}`, document.querySelector("form#addTDForm input#newText").value);
+    addToList(localStorage.getItem("numOfItems"));
     localStorage.setItem("numOfItems", parseInt(localStorage.getItem("numOfItems"))+1);
-    localStorage.setItem(`info${parseInt(localStorage.getItem("numOfItems"))-1}`, document.querySelector("form#addTDForm input#newText").value);
-    addToList(localStorage.getItem("numOfItems")-1);
+    console.log(localStorage.getItem("numOfItems"));
     textBox.value = "";
 }
 //Adds list Items from local Storage to the page
@@ -43,10 +55,13 @@ myList.addEventListener("click", function(evt){
     if(evt.target.tagName === 'INPUT'){
         if(evt.target.value == "complete"){
             let myIndex = Array.prototype.indexOf.call(myList.children, evt.target.parentElement);
-            clickedComplete(myIndex)
+            clickedComplete(myIndex);
+        }else if(evt.target.value == "Not Done"){
+            let myIndex = Array.prototype.indexOf.call(myList.children, evt.target.parentElement);
+            clickedUndo(myIndex);
         }else{
             let myIndex = Array.prototype.indexOf.call(myList.children, evt.target.parentElement);
-            clickedUndo(myIndex)
+            clickedRemove(myIndex);
         }
     }
 })
